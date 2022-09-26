e138cd256f7da5489397de2a41a0da864e9bbe67
潘一航 <11665009+pan-yihang@user.noreply.gitee.com>
2022-09-26 09:23:40 +0800

    denglu

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,21 @@
 import axios from 'axios'
+import { Message, message } from 'element-ui'
 const service = axios.create({
-  baseUrl: process.env.VUE_APP_BASE_API,
+  baseURL: process.env.VUE_APP_BASE_API,
   timeout:5000,
 })
+service.interceptors.response.use(response => {
+  const { success, message, data } = response.data
+  if (success) {
+    return data
+  } 
+    Message.error(message)
+    return Promise.reject(new Error(message))
+  
+}, error => {
+    Message.error(error.message)
+    return Promise.reject(error)
+})
+
+
 export default service
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,31 +2,20 @@
   <div class="login-container">
     <el-form
       ref="loginForm"
-      :model="loginForm"
-      :rules="loginRules"
       class="login-form"
       auto-complete="on"
       label-position="left"
+      :model="loginForm"
+      :rules="rules"
     >
       <div class="title-container">
         <h3 class="title">
           <img src="@/assets/common/login-logo.png" alt="" />
         </h3>
       </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
+      <el-form-item prop="mobile">
+        <span class="svg-container el-icon-user-solid" />
+        <el-input placeholder="请输入手机号码" v-model="loginForm.mobile" />
       </el-form-item>
 
       <el-form-item prop="password">
@@ -34,114 +23,103 @@
           <svg-icon icon-class="password" />
         </span>
         <el-input
-          :key="passwordType"
-          ref="password"
+          ref="pwd"
           v-model="loginForm.password"
           :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
+          placeholder="请输入密码"
         />
-        <span class="show-pwd" @click="showPwd">
+        <span class="svg-container" @click="showPwd">
           <svg-icon
             :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
           />
         </span>
       </el-form-item>
-
       <el-button
-        :loading="loading"
-        type="primary"
+        class="loginBtn"
         style="width: 100%; margin-bottom: 30px"
-        @click.native.prevent="handleLogin"
+        :loading="loading"
+        @click="login"
         >Login</el-button
       >
-
       <div class="tips">
-        <span style="margin-right: 20px">username: admin</span>
-        <span> password: any</span>
+        <span style="margin-right: 20px">账号: 13800000002</span>
+        <span> 密码: 123456</span>
       </div>
     </el-form>
   </div>
+  <!-- 环境变量的作用
+  1. 正常公司中 有几个环境 4 开发 dev 测试 test 预发 uat 线上 pro
+  2. 在项目里如何配置这几个环境  通过 .env 配置 base api
+  开发环境的接口前缀 /api
+  线上环境的接口前缀 /prod-api
+   -->
 </template>
-
 <script>
-import { validUsername } from "@/utils/validate";
-
+import { validPhone } from "@/utils/validate";
+import { login } from "@/api/login";
 export default {
   name: "Login",
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error("Please enter the correct user name"));
-      } else {
-        callback();
-      }
-    };
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error("The password can not be less than 6 digits"));
+    const phoneValid = (rule, value, callback) => {
+      if (!validPhone(value)) {
+        callback(new Error("手机格式错误"));
       } else {
         callback();
       }
     };
     return {
-      loginForm: {
-        username: "admin",
-        password: "111111",
-      },
-      loginRules: {
-        username: [
-          { required: true, trigger: "blur", validator: validateUsername },
-        ],
-        password: [
-          { required: true, trigger: "blur", validator: validatePassword },
-        ],
-      },
       loading: false,
       passwordType: "password",
-      redirect: undefined,
+      loginForm: {
+        mobile: "1382",
+        password: "123456",
+      },
+      rules: {
+        mobile: [
+          {
+            required: true,
+            message: "手机号必填",
+            trigger: "blur",
+          },
+          {
+            validator: phoneValid,
+            // pattern:
+            //   /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[189]))\d{8}$/,
+
+            // message: "手机格式不对",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "密码必填",
+            trigger: "blur",
+          },
+          { min: 6, max: 16, message: "密码格式不对", trigger: "blur" },
+        ],
+      },
     };
   },
-  watch: {
-    $route: {
-      handler: function (route) {
-        this.redirect = route.query && route.query.redirect;
-      },
-      immediate: true,
-    },
-  },
+
   methods: {
     showPwd() {
-      if (this.passwordType === "password") {
-        this.passwordType = "";
-      } else {
-        this.passwordType = "password";
-      }
+      this.passwordType === "password"
+        ? (this.passwordType = "")
+        : (this.passwordType = "password");
       this.$nextTick(() => {
-        this.$refs.password.focus();
+        this.$refs.pwd.focus();
       });
     },
-    handleLogin() {
-      this.$refs.loginForm.validate((valid) => {
-        if (valid) {
-          this.loading = true;
-          this.$store
-            .dispatch("user/login", this.loginForm)
-            .then(() => {
-              this.$router.push({ path: this.redirect || "/" });
-              this.loading = false;
-            })
-            .catch(() => {
-              this.loading = false;
-            });
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
+    async login() {
+      try {
+        await this.$refs.loginForm.validate();
+        this.loading = true;
+        await this.$store.dispatch("user/loginAction", this.loginForm);
+      } finally {
+        this.loading = false;
+        this.$router.push("/");
+      }
     },
   },
 };
@@ -151,8 +129,8 @@ export default {
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg: #283443;
-$light_gray: #68b0fe; // 将输入框颜色改成蓝色
+$bg: #d3e4ff;
+$light_gray: #68b0fe;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -165,10 +143,6 @@ $cursor: #fff;
 .login-container {
   background-image: url("~@/assets/common/login.jpg"); // 设置背景图片
   background-position: center; // 将图片位置设置为充满整个屏幕
-
-  .el-form-item__error {
-    color: #fff;
-  }
   .el-input {
     display: inline-block;
     height: 47px;
@@ -190,11 +164,24 @@ $cursor: #fff;
       }
     }
   }
+
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
     // background: rgba(0, 0, 0, 0.1);
     border-radius: 5px;
     color: #454545;
+    background: rgba(255, 255, 255, 0.7); // 输入登录表单的背景色
+  }
+  .el-form-item__error {
+    color: #fff;
+  }
+  .loginBtn {
+    background: #407ffe;
+    height: 64px;
+    line-height: 32px;
+    font-size: 24px;
+    border: none;
+    color: #fff;
   }
 }
 </style>
@@ -202,7 +189,7 @@ $cursor: #fff;
 <style lang="scss" scoped>
 $bg: #2d3a4b;
 $dark_gray: #889aa4;
-$light_gray: #eee;
+$light_gray: #68b0fe;
 
 .login-container {
   min-height: 100%;
@@ -260,11 +247,5 @@ $light_gray: #eee;
     cursor: pointer;
     user-select: none;
   }
-}
-.el-form-item {
-  border: 1px solid rgba(255, 255, 255, 0.1);
-  background: rgba(255, 255, 255, 0.7); // 输入登录表单的背景色
-  border-radius: 5px;
-  color: #454545;
 }
 </style>
